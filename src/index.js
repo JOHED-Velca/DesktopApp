ca2b78387eb3f93f5a4146582245237e8c3fa25b
Jonathan Velasquez <johedvelca@gmail.com>
2025-05-04 20:14:58 -0400

    code copied from source repo and started debugging it.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
+const electron = require("electron");
+const { app, BrowserWindow } = electron;
+const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
@@ -13,8 +14,9 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      preload: path.join(__dirname, 'preload.js'),
-    },
+      contextIsolation: false,
+      enableRemoteModule: true,
+    }
   });
 
   // and load the index.html of the app.
@@ -27,24 +29,22 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
+app.on('ready', createWindow);
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
   }
 });
 
--- src/render.js
+++ src/render.js
@@ -1,88 +1,110 @@
-//Buttons
+const { desktopCapturer, remote } = require('electron');
+
+const { writeFile } = require('fs');
+
+const { dialog, Menu } = require("electron").remote;
+
+// Global state
+let mediaRecorder; // MediaRecorder instance to capture footage
+const recordedChunks = [];
+
+// Buttons
 const videoElement = document.querySelector('video');
+
 const startBtn = document.getElementById('startBtn');
+startBtn.onclick = e => {
+    var num=0;
+    console.log("The count is: " + num)
+  mediaRecorder.start();
+  startBtn.classList.add('is-danger');
+  startBtn.innerText = 'Recording';
+};
+
 const stopBtn = document.getElementById('stopBtn');
+
+stopBtn.onclick = e => {
+  mediaRecorder.stop();
+  startBtn.classList.remove('is-danger');
+  startBtn.innerText = 'Start';
+};
+
 const videoSelectBtn = document.getElementById('videoSelectBtn');
 videoSelectBtn.onclick = getVideoSources;
 
-const { desktopCapturer, remote } = require('electron');
-const { Menu } = remote;
-
-//Get the available video sources
+// Get the available video sources
 async function getVideoSources() {
-    const inputSources = await desktopCapturer.getSources({
-        types: ['window', 'screen']
-    });
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
 
-    const videoOptionsMenu = Menu.buildFromTemplate(
-        inputSources.map(source => {
-            return {
-                label: source.name,
-                click: () => selectSource(source)
-            };
-        })
-    );
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name,
+        click: () => selectSource(source)
+      };
+    })
+  );
 
-    videoOptionsMenu.popup();
+
+  videoOptionsMenu.popup();
 }
 
-let mediaRecorder;
-const recordedChunks = [];
-
-//Change the videoSourcee window to record
+// Change the videoSource window to record
 async function selectSource(source) {
-    videoSelectBtn.innerText = source.name;
 
-    const constraints = {
-        audio: false,
-        video: {
-            mandatory: {
-                chromeMediaSource: 'desktop',
-                chromeMediaSourceId: source.id
-            }
-        }
-    };
+  videoSelectBtn.innerText = source.name;
 
-    //create a Stream
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-
-    //Preview the source in a video element
-    videoElement.srcObject = stream;
-    videoElement.play();
-
-    //Create the media recorder
-    const options = { mimeType: 'video/webm; codecs=vp9'};
-    mediaRecorder = new MediaRecorder(stream, options);
-
-    //Register event handlers
-    mediaRecorder.ondataavailable = handleDataAvailable;
-    mediaRecorder.onstop = handleStop;
-
-    //Capture all recorded chunks
-    function handleDataAvailable(e) {
-        console.log('video data available');
-        recordedChunks.push(e.data);
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id
+      }
     }
+  };
 
-    const { dialog } = remote;
+  // Create a Stream
+  const stream = await navigator.mediaDevices
+    .getUserMedia(constraints);
 
-    const { writeFile } = require('fs');
+  // Preview the source in a video element
+  videoElement.srcObject = stream;
+  videoElement.play();
 
-    //Saves the video file on stop
-    async function handleStop(e) {
-        const blob = new Blob(recordedChunks, {
-            type: 'vodep/webm; codecs=vp9'
-        });
+  // Create the Media Recorder
+  const options = { mimeType: 'video/webm; codecs=vp9' };
+  mediaRecorder = new MediaRecorder(stream, options);
 
-        const buffer = Buffer.from(await blob.arrayBuffer());
+  // Register Event Handlers
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
 
-        const { filePath } = await dialog.showSaveDialog({
-            buttonLabel: 'Save video',
-            defaultPath: `vid-${Date.now()},webm`
-        });
+  // Updates the UI
+}
 
-        console.log(filePath);
+// Captures all recorded chunks
+function handleDataAvailable(e) {
+  console.log('video data available');
+  recordedChunks.push(e.data);
+}
 
-        writeFile(filePath, buffer, () => console.log('video saved successfully!'));
-    }
+// Saves the video file on stop
+async function handleStop(e) {
+  const blob = new Blob(recordedChunks, {
+    type: 'video/webm; codecs=vp9'
+  });
+
+  const buffer = Buffer.from(await blob.arrayBuffer());
+
+  const { filePath } = await dialog.showSaveDialog({
+    buttonLabel: 'Save video',
+    defaultPath: `vid-${Date.now()}.webm`
+  });
+
+  if (filePath) {
+    writeFile(filePath, buffer, () => console.log('video saved successfully!'));
+  }
+
 }
